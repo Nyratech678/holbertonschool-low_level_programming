df06261f337c9319bb5e786c6f26a2cc8f4ca3e4
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-06-17 12:05:50 +0000

    echo

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,20 +1,6 @@
-#include "main.h"
-/**
- * main - Entry point
- *
- * Description: Prints "_putchar" foolowed by a new line.
- *
- * Return: Alway 0 (Success)
- */
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-return (0);
-}
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
