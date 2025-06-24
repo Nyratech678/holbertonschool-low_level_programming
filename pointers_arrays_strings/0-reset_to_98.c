b7df17f0e53a07c9e2f8d7050dc313c5f8fa220d
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-06-24 08:06:48 +0000

    echo

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of the integer it points to to 98.
+ * @n: Pointer to an int to be updated.
+ *
+ * Return: Nothing (void).
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
