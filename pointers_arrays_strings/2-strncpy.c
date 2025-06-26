286ba322766130ff97a3573830e20391b0ac18e0
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-06-26 09:04:18 +0000

    echo

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
  * _strncpy - Copies a string, including the terminating null byte,
  *            using at most an inputted number of bytes.
  *            If the length of src is less than n, the remainder of dest
  *            is filled with null bytes.
- *            Works identically to the standard library function `strncpy`.
+ *            Works identically to the standard library function `_strncpy`.
  * @dest: Buffer storing the string copy
  * @src: Source string
  * @n: Maximum number of bytes to copy
