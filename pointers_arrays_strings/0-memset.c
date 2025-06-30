d00a7186455dc0377815b920c42e5eda0d796ac4
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-06-30 12:57:41 +0200

    Add memory manipulation functions and update main files for testing
    - Implemented _memset and _memcpy functions for memory operations.
    - Added simple_print_buffer function to display buffer contents in hexadecimal.
    - Updated main files to test new functions and demonstrate their usage.
    - Added function prototypes in main.h for new functions.
    - Implemented _strchr and _strspn functions for string manipulation.

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills the first n bytes of the memory area
+ *           pointed to by s with the constant byte b
+ * @s: pointer to the memory area to fill
+ * @b: constant byte to fill with
+ * @n: number of bytes to fill
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+    while (n)
+    {
+        s[n - 1] = b;
+        n--;
+    }
+    return (s);
+}
---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Returns a pointer to the first occurrence
+ *           of the character c in the string s, or NULL if the
+ *           character is not found
+ * @s: string targeted
+ * @c: character targeted
+ *
+ * Return: pointer to first occurrence of c or NULL
+ */
+char *_strchr(char *s, char c)
+{
+    int i;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (s[i] == c)
+            return (s + i);
+    }
+    if (c == '\0')
+        return (s + i);
+    return (NULL);
+}
