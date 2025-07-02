5aaad5345e3cf26f8e167af4ba4bbee9686f6d29
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-07-02 11:42:08 +0000

    echo

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * _memset - fills the first n bytes of the memory area
- *           pointed to by s with the constant byte b
- * @s: pointer to the memory area to fill
- * @b: constant byte to fill with
- * @n: number of bytes to fill
+ * _memset - Fills the first n bytes of the memory area
+ *           pointed to by s with the constant byte b.
+ * @s: Pointer to the memory area to fill.
+ * @b: Constant byte to fill with.
+ * @n: Number of bytes to fill.
  *
- * Return: pointer to the memory area s
+ * Return: Pointer to the memory area s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    while (n)
-    {
-        s[n - 1] = b;
-        n--;
-    }
-    return (s);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
 }
