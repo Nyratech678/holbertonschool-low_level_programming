3c7cda1b62f46c6341852adbdea7b32c7b8938d5
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-07-02 11:54:08 +0000

    echo

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -2,27 +2,26 @@
 #include <stddef.h>
 
 /**
- * _strpbrk - locates the first occurrence in the string s
- *            of any of the bytes in the string accept
- * @s: string where search is made
- * @accept: string where searched bytes are located
+ * _strpbrk - Locates the first occurrence in the string s
+ *            of any of the bytes in the string accept.
+ * @s: String where search is made.
+ * @accept: String where searched bytes are located.
  *
- * Return: pointer to the byte in s that matches one of the bytes in accept,
- *         or NULL if no such byte is found
+ * Return: Pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-            {
-                return (s + i);
-            }
-        }
-    }
-    return (NULL);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s + i);
+		}
+	}
+
+	return (NULL);
 }
