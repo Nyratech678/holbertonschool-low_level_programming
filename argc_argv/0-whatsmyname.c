a98f315981d03c25c59155d729c12713ef1da642
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-07-03 11:15:32 +0200

    echo

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc: argument count (unused)
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+    (void)argc;
+
+    printf("%s\n", argv[0]);
+
+    return (0);
+}
+
