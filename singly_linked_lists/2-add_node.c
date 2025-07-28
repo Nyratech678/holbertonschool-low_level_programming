646021aa70e8bdabe2ecf5fbafce7004be5e0109
Nyratech678 <uwuchocolatemilk09876@proton.me>
2025-07-28 12:20:35 +0200

    echo*

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,17 +1,20 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdio.h>
 /**
-* add_node - ajoute un nouveau noeud au début d'une list_t
-* @head: adresse du pointeur vers la tête de la liste
-* @str: chaîne à dupliquer et à ajouter à la liste
-*
-* Return: adresse du nouveau noeud ou NULL en cas d'échec
-*/
+ * add_node - Adds a new node at the beginning of a list_t list
+ * @head: double pointer to the head node of the list
+ * @str: string to duplicate and store in the new node
+ * Return: address of the new element, or NULL if it failed
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
@@ -24,9 +27,12 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
+	new->len = strlen(str);
+
 	new->next = *head;
 	*head = new;
 
 	return (new);
 }
+
 
